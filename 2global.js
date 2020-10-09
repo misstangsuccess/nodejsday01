e61493bd75165f604da629511abbca3571aaf100
misstangsuccess <1244669321@qq.com>
2020-10-09 17:49:49 +0800

    add:global

---
+++ 2global.js
@@ -0,0 +1,8 @@
+//console.log(global);//global作为nodejs的顶层对象(在nodejs环境运行)
+//console.log(global.process);
+//console.log(global.Buffer);
+//global可以省略
+//console.log(Buffer);
+
+//nodejs没有window对象
+console.log(window);
